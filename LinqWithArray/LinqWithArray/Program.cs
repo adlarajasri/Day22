9da24c298b3c519f3589a98121d80d640b10a9a0
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-08 09:43:20 +0530

    Add files via upload

---
+++ LinqWithArray/LinqWithArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWithArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] flowers = { "dahila", "rose", "lotus" };
+            var fQuery =
+               from flower in flowers
+               let len=flower.Length
+               where len>5 && len<7
+                where (flower.StartsWith("l"))
+                select flower;
+            foreach (var flower in fQuery) 
+            {
+                Console.WriteLine(flower);
+            }
+        }
+    }
+}
